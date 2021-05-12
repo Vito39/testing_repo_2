91f1149926c9dbe9133e1aa078b9082046dbb759
kunalx90x90 <kunal.sharma@elucidata.io>
2021-05-12 13:31:33 +0530

    Error handling when logging for first time

--- polly_python/__init__.py
+++ polly_python/__init__.py
@@ -1,3 +1,4 @@
+from polly_python.exceptions import MalFormedConfigurationException
 from polly_python.session import PollySession
 
 """Top-level package for polly_python."""
@@ -13,8 +14,11 @@ def setup_default_session():
     """
     Creates default session for polly python sdk
     """
-    global DEFAULT_SESSION
-    DEFAULT_SESSION = PollySession()
+    try:
+        global DEFAULT_SESSION
+        DEFAULT_SESSION = PollySession()
+    except MalFormedConfigurationException:
+        pass
 
 
 def _get_default_session():
