1ad5be0a8e3c4ba749b6274db1f19cc70fc64a48
Bisweswar Martha <bisweswar.martha@elucidata.io>
2021-06-29 18:06:09 +0530

    changed acc. to new endpoints

--- examples/omixatlas_example.py
+++ examples/omixatlas_example.py
@@ -1,59 +1,71 @@
 from polly.omixatlas import OmixAtlas
+from json import dumps
 
-REFRESH_TOKEN = 'eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.JyuHPGvz_li1zrq8b4gL6NcyvUiU4KLS9oJgLgk4d-CZGJFLvBXyIXqhyf5jP3qSkZb002TM9bhXNwhVjTjrPEjtctf30Xnjf7RidSF9hPy4GowAL_t4IJDBMqzQyYdyqE6YOWZ1tBHa_v422iYdeXuFnD4sDOYTAUqGB_6VnxbrMuDJSG5vsNldg95UNnX1nefO6b5b9eUcvTyRfE2KbKVmRWcHv07JygxXeICq-mGx0hT5NYz_JRTKo68pZodwpKSlx4NbdI5PpSOrtjWCvTVLBqxnSIuqYQP9MsHoae3v7JAxrbom5MMkKLHFWpyjC2RlCkgiAwpOUX7LRIfpig.n9ws8UTPY16nu8PA.S9EHy6Z036H9vPudfmJ8vHSbGozUckyEhkP3krC7SQtvuNtd5Tzy-nMR2HcUvNQAGRcBRXJrfmBgKU_NBxioc3u6DRxjQ_22w0A3ESEDHOe2iHUagEMf1RGmp68hXQmz_PrI6UAvSff_7bpjkTmBWZpIDb1_GWZH6B8Ob8c9rhiRZbXK5vx4Dp-EOFDT_xRUAxCtF3NC0zhvanDvAfyrsdzMpPtkCWcglWw_cyIPKQ-vCB7dtz67P1ZGZ4IdC0BNSKCLIKkJbMTAnObe5nsESL2cUISwcWBiZkdj-NgkKpf27CDVy88VxTBWvscHO8fUE7YtftTeju8QnUyCur7chMlMU_Be_kqfNOGlMBYkqo8P2TJAttunnHRgvasLCMdj489ppW_Nm8piXpkjHqO90xTOzn80XlA1k2enaKpklJYOUs-wcxsuyXXPiDuL2KHqzIdkFUfwh1khM7prj5JtEbwmGkw68iJgJtLGTWFTVV3h1dfmtzG7jced1K9flbsNqsrUdmce6wKnedMUqWLPdhX46KNcq6tFHsAiuRjSlTXmkcjJak3Tf603cyCR8OCKHwUWF-nERNn9DsK2GHvje0K3cBH5i6k1ZgqAqU2rIaSYZagcG5zWR7vEKuApmdR8z-pyfY0d4LM_sdPorlCYdQS074_y4hYX7VYdyfLzTn7opi3SWLCuFZiJjic9dhL0NCi5l94rMm2UygPag21z7pF7MZfq41HEJSsiVDHJlSOYWN0XwGU6gZOQg8Wlo3UXLfN4W9ZGeC9yYII9BWBpQktmFV6lj-r1czgxiIPGJ8x_Jr0sR4omdb3ZMi5DwpgzmMNc-V5RKp2KHiVA7tcVCb3i3AKuMItK7asbohgPKCgMtFeF8feiGOyl506AzSBOPK_FyNpauCYhP_LOLuhsXHq9_sv6duto5p7WV2MYs5rCIKdaDCiu4h5jaQznJjpMRCPTE0LuFC7hS4zNsQNKt0C_BZkebv2V6obSzLOQ3hw6FsRJW-FGybNGnKZig4q3TnLXuDKUi80BztIOf3EFr5swCk9y0VCDtiiUWVNLwVSvoBupPLYVJKL1_rF9b_bNb4RAGTMJ31yo0K8AxG0eAMzghqajGynDfzgFOHYGo-QAugnnNTlhbbZAFd10LWpJbf1iEKjjlhImL_YFdSCNfmJsABn2ihobNAI4lCfU4UTgQ9gmOq77GY2C7oddAvKaJPzjPPi4ksW4Os2NPeO7lSfkkIcMrEzyjokZzZh3f7svOeLGrLza4Rdln37XsWGeu-a6FbqZ0EeiplNshrz_U9Qp4v5fwJoPyoeIz814SqTiKavVjBve1lRL2DU.cQiJ-215_ZS_fUMj7Wp-Sw'
+LOG_FILE = "output.log"
+open(LOG_FILE, "w").close()
+REFRESH_TOKEN = "eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.gzTcATBCijjG7tDI-lH5euZeWxCPuamqgG6tBVfD-c5mZdWRi2s1whrhahIs5jGNYMqiE31CQvkdtZbVuJlzhVjuH353jZPiE6WJKkPUffCmMD3Kb9J0OY8iYtWEmRN3-Vql3KyTrDW0h6JDJUl20Fv86GMydXL3vY3Wh8ftqErLPD9hiDVnbvHBSlsA3TxJaIzHCIJHAIZjoMDD-H2R8vkSq9S33soJ1SNWZv6UgkXpHOr8xjxfQTCG5RQ63tVF8QGQepMZwt_mwxUzrFy4Ci_A3cY9glijuIQ0Gzc0jOoBzYwzdmIx-8uu03fW_VfayitmT-5oH_dgeEjud9VYjQ.wIe42sIoFNBrjXlI.S6J9XZshsoOoYrTkZTnnPga0LsjIv_m-aAAOYgzXlqxLiTbiQJhiI55MBC0ZYXs_-1dtSQNImmASFKuTSwbbgMqBaB7EOr8rxuifWZRVlkuH0HKw_hg_FRdufh04uHJ-7ZiXE5GWaj5iLiNvTQ42tkufQbbcRrlbPFRR7LmPaGDc_ujktg8S2kI6D2cC0iZgiD7ZPOPNUy5zemEv1k-YdoeANss4btYQGtOJAoyTeWnPN3H983w4Xjrox7rrg5ZJztTg5SWwk5oWgFBxY_OZ9EeHL01jMS9nsW4kOIPHieAWVTbWKOioAJu1UCuoemeRLv8SjXz8uPE2nW5W9NGNzQtQXChsfQyIw_PrRXWPQ8-VcQR1Cf5g-hYjF-bhauE3iN8YiMdGSBm-pw-IzzSE6PbK5_0-AH_AfqEuNjwlskbFHu3t5onJmibGmbLbbWcJCB6XKdOXhKAU6gu_tvts1boR2KioD3BDpu41Zuw6bxr2-qmE5DG0xfwKSvH7xebRatXeQ4I9uJ4itHsXlMJsxDVNCr3V_V3QmKiV2dwICEcs-jXXcG8TQrNoGeavMe4BwvzuPxTTjEUCkTzWd8j2oNKuUHnOh89j6Oc27MIVlSqusrLlO8M9MCmbs6hnLPDxk5oXbPWL5WCJ85hmg-C_5ehup59sYMdehDqOL-u8GNAwrlMPftdEM-mtq8jgg2UShB1q6pP1fOmyTZ7_iokT4yXX2RD9ljFY42HW0ra0Vz30T-dXSqGeBqJc9q27imRbORG7mu2eTkJSqa0bIspDxk_CQerb9jF0sX6LrbcrMnqQTBt5P6HB74256SA5Hl2mYCggB89vhJpUJUc6I-CR8Z2i-fa67MwyQILehNfPTF0YZGd8qBG09tJ1kKotdbCcnt-Vl_Bi-smOU3gmVacG0Tt-qywZV5qcG5DLG9w0S7Jyj-YWFDARwzyHQZp3K7zqDM-tkJEG4Q7xXoUcvyY_uHEQwG6hb-ZkvBHdKB8mi1SJwMWWeGHZRUDQCesUaOl8BFlVv87sFqX7X8P_NrDqpP-2IfpNRnbv_2CCD1J0VUP5WnjMHp7C-fdENRXCk2e0nmRyeWNbft4eau5ztfS2o9hzY3y0rDU8EVq8qN9W48vGAdSr7iroYQCnOACo5nbIF8iFos3-w3CJAC7xY3BLKLKW9oOe4hxxhW6SU7nWZL0l0dw6fPHt8-nFbydksC-vzlCtMa6ZHyL9Kf4l28z6h06wrqwh8tpTQ0k9_DvMGecavgTjQ2K1ZORWu8auBb9t9jLu2rebkwFxj0RpoTIDS0OV1aFXDoOogjYOYQpRYFf0RujizZT9CMjDs4C7bxYdvUjgb3AK0fLm.odTPJZMI77N1XB80R6pr0Q"
 
 repo_client = OmixAtlas(REFRESH_TOKEN)
 
-# Get all Repos
-# all_repos = repo_client.get_all_repos()
-# print(all_repos)
+### Get All Omixatlases
 
-# Get all packages for single repo
-# repo_16_packages = repo_client.get_all_packages(repo_id=16)
-# print(repo_16_packages)
+all_omixatlases = repo_client.get_all_omixatlas()
 
-# # Create New Package
-# new_package = repo_client.create_package(repo_id=16)
+with open(LOG_FILE, "a") as logfile:
+    logfile.write("\n\nAll Omixatlases")
+    logfile.write(dumps(all_omixatlases, indent=4))
 
-# # Get New Package Data
-# new_package_name = somehow_get_package_name(new_package)
-# new_package_data = repo_client.get_package(repo_id=16,
-#                                            package=new_package_name)
+### Get Omixatlas by Name
 
-# # Delete New Package
-# repo_client.delete_package(repo_id=16, package=new_package_name)
+repo_name = "elucidata.test_omixwiki"
 
-# Elastic Search
+repo_details = repo_client.get_omixatlas_details(repo_name)
+
+with open(LOG_FILE, "a") as logfile:
+    logfile.write("\n\n Omixatlas Details with Name\n")
+    logfile.write(dumps(repo_details, indent=4))
+
+### Get Omixatlas by Name
+
+repo_id = "1624966495340"
+
+repo_details = repo_client.get_omixatlas_details(repo_name)
+
+with open(LOG_FILE, "a") as logfile:
+    logfile.write("\n\n Omixatlas Details with ID\n")
+    logfile.write(dumps(repo_details, indent=4))
+
+### SQL Queries
+
+query = "SELECT * FROM test_files"
+
+query_response = repo_client.query_metadata(query)
+
+with open(LOG_FILE, "a") as logfile:
+    logfile.write("\n\n Metadata Query\n")
+    logfile.write(dumps(query_response, indent=4))
+
+### Elastic Search Queries
 
 query = {
-    "size": 100,
-    "sort": [{
-        "dataset_id.keyword": "asc"
-    }],
-    "query": {
-        "bool": {
-            "must": [{
-                "exists": {
-                    "field": "dataset_id"
-                }
-            }, {
-                "bool": {
-                    "should": [{
-                        "term": {
-                            "disease.keyword": "obesity"
-                        }
-                    }, {
-                        "term": {
-                            "disease.keyword": "fibrosis"
-                        }
-                    }]
-                }
-            }]
-        }
+    "bool": {
+        "must": [
+            {"term": {"_index": "test_files"}},
+            {"term": {"kw_filetype.keyword": "png"}},
+        ]
     }
 }
 
-# Works now
-response = repo_client.elastic_query(index='liveromix_atlas_files',
-                                     query=query)
-print(response)
+search_response = repo_client.search_metadata(query)
+
+with open(LOG_FILE, "a") as logfile:
+    logfile.write("\n\n Metadata Query\n")
+    logfile.write(dumps(search_response, indent=4))
+
+### Download Data
+
+data_id = search_response["hits"]["hits"][0]["_id"]
+
+response = repo_client.download_data(repo_name, data_id)
+signed_url = response.get("data")
+print(signed_url)
--- polly/omixatlas.py
+++ polly/omixatlas.py
@@ -1,61 +1,44 @@
-from .core import PostPython
-from json import dumps
-import pathlib
-
-curr_path = pathlib.Path(__file__).parent.absolute()
-
-COLLECTION_PATH = f'{curr_path}/collections/omixatlas/repository_packages.postman_collection.json'
-ENVIRONMENT_PATH = f'{curr_path}/collections/omixatlas/repositories.postman_environment.json'
+import requests
 
 
-class OmixAtlas():
-    def __init__(self, refresh_token) -> None:
-        self.refresh_token = refresh_token
-        self.runner = PostPython(COLLECTION_PATH)
-        self.runner.environments.load(ENVIRONMENT_PATH)
-        self.runner.environments.update({'refreshToken': refresh_token})
+class OmixAtlas:
+    def __init__(self, refresh_token: str) -> None:
+        self.base_url = "https://v2.api.testpolly.elucidata.io/v1/omixatlases"
+        self.headers = {
+            "Content-Type": "application/vnd.api+json",
+            "Cookie": f"refreshToken={refresh_token}",
+        }
 
-    def get_all_repos(self):
-        response = self.runner.Requests.repositories_get_all()
+    def get_all_omixatlas(self):
+        url = self.base_url
+        response = requests.get(url, headers=self.headers)
         response.raise_for_status()
         return response.json()
 
-    def get_all_packages(self, repo_id):
-        self.runner.environments.update({'repositoryId': repo_id})
-        response = self.runner.Requests.repository_package_get_all()
+    def get_omixatlas_details(self, key: str):
+        url = f"{self.base_url}/{key}"
+        response = requests.get(url, headers=self.headers)
         response.raise_for_status()
         return response.json()
 
-    def get_package(self, repo_id, package):
-        self.runner.environments.update({
-            'repositoryId': repo_id,
-            'packageNametoCreate': package
-        })
-        response = self.runner.repository_package_get_one()
+    def query_metadata(self, query: str):
+        url = f"{self.base_url}/_query"
+        payload = {"query": query}
+        response = requests.get(url, headers=self.headers, json=payload)
         response.raise_for_status()
         return response.json()
 
-    def create_package(self, repo_id):
-        self.runner.environments.update({'repositoryId': repo_id})
-        response = self.runner.Requests.repository_package_create()
+    # ? DEPRECATED
+    def search_metadata(self, query: dict):
+        url = f"{self.base_url}/_search"
+        payload = {"query": query}
+        response = requests.get(url, headers=self.headers, json=payload)
         response.raise_for_status()
         return response.json()
 
-    def delete_package(self, repo_id, package):
-        self.runner.environments.update({
-            'repositoryId': repo_id,
-            'packageNametoCreate': package
-        })
-        response = self.runner.Requests.repository_package_delete()
-        response.raise_for_status()
-        return response
-
-    def elastic_query(self, index, query):
-        self.runner.environments.update({
-            'index': index,
-            'query': dumps(query)
-        })
-
-        response = self.runner.Requests.elastic_search()
+    def download_data(self, repo_name, _id: str):
+        url = f"{self.base_url}/{repo_name}/download"
+        params = {"_id": _id}
+        response = requests.get(url, headers=self.headers, params=params)
         response.raise_for_status()
         return response.json()
