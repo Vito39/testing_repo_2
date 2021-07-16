286b8df0c505ffac1d004833cc8507e50116a914
Bisweswar Martha <bisweswar.martha@gmail.com>
2021-07-16 10:46:49 +0530

    Moved to production polly, improved error handling

--- examples/omixatlas_example.py
+++ examples/omixatlas_example.py
@@ -3,69 +3,84 @@ from json import dumps
 
 LOG_FILE = "output.log"
 open(LOG_FILE, "w").close()
-REFRESH_TOKEN = "eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.gzTcATBCijjG7tDI-lH5euZeWxCPuamqgG6tBVfD-c5mZdWRi2s1whrhahIs5jGNYMqiE31CQvkdtZbVuJlzhVjuH353jZPiE6WJKkPUffCmMD3Kb9J0OY8iYtWEmRN3-Vql3KyTrDW0h6JDJUl20Fv86GMydXL3vY3Wh8ftqErLPD9hiDVnbvHBSlsA3TxJaIzHCIJHAIZjoMDD-H2R8vkSq9S33soJ1SNWZv6UgkXpHOr8xjxfQTCG5RQ63tVF8QGQepMZwt_mwxUzrFy4Ci_A3cY9glijuIQ0Gzc0jOoBzYwzdmIx-8uu03fW_VfayitmT-5oH_dgeEjud9VYjQ.wIe42sIoFNBrjXlI.S6J9XZshsoOoYrTkZTnnPga0LsjIv_m-aAAOYgzXlqxLiTbiQJhiI55MBC0ZYXs_-1dtSQNImmASFKuTSwbbgMqBaB7EOr8rxuifWZRVlkuH0HKw_hg_FRdufh04uHJ-7ZiXE5GWaj5iLiNvTQ42tkufQbbcRrlbPFRR7LmPaGDc_ujktg8S2kI6D2cC0iZgiD7ZPOPNUy5zemEv1k-YdoeANss4btYQGtOJAoyTeWnPN3H983w4Xjrox7rrg5ZJztTg5SWwk5oWgFBxY_OZ9EeHL01jMS9nsW4kOIPHieAWVTbWKOioAJu1UCuoemeRLv8SjXz8uPE2nW5W9NGNzQtQXChsfQyIw_PrRXWPQ8-VcQR1Cf5g-hYjF-bhauE3iN8YiMdGSBm-pw-IzzSE6PbK5_0-AH_AfqEuNjwlskbFHu3t5onJmibGmbLbbWcJCB6XKdOXhKAU6gu_tvts1boR2KioD3BDpu41Zuw6bxr2-qmE5DG0xfwKSvH7xebRatXeQ4I9uJ4itHsXlMJsxDVNCr3V_V3QmKiV2dwICEcs-jXXcG8TQrNoGeavMe4BwvzuPxTTjEUCkTzWd8j2oNKuUHnOh89j6Oc27MIVlSqusrLlO8M9MCmbs6hnLPDxk5oXbPWL5WCJ85hmg-C_5ehup59sYMdehDqOL-u8GNAwrlMPftdEM-mtq8jgg2UShB1q6pP1fOmyTZ7_iokT4yXX2RD9ljFY42HW0ra0Vz30T-dXSqGeBqJc9q27imRbORG7mu2eTkJSqa0bIspDxk_CQerb9jF0sX6LrbcrMnqQTBt5P6HB74256SA5Hl2mYCggB89vhJpUJUc6I-CR8Z2i-fa67MwyQILehNfPTF0YZGd8qBG09tJ1kKotdbCcnt-Vl_Bi-smOU3gmVacG0Tt-qywZV5qcG5DLG9w0S7Jyj-YWFDARwzyHQZp3K7zqDM-tkJEG4Q7xXoUcvyY_uHEQwG6hb-ZkvBHdKB8mi1SJwMWWeGHZRUDQCesUaOl8BFlVv87sFqX7X8P_NrDqpP-2IfpNRnbv_2CCD1J0VUP5WnjMHp7C-fdENRXCk2e0nmRyeWNbft4eau5ztfS2o9hzY3y0rDU8EVq8qN9W48vGAdSr7iroYQCnOACo5nbIF8iFos3-w3CJAC7xY3BLKLKW9oOe4hxxhW6SU7nWZL0l0dw6fPHt8-nFbydksC-vzlCtMa6ZHyL9Kf4l28z6h06wrqwh8tpTQ0k9_DvMGecavgTjQ2K1ZORWu8auBb9t9jLu2rebkwFxj0RpoTIDS0OV1aFXDoOogjYOYQpRYFf0RujizZT9CMjDs4C7bxYdvUjgb3AK0fLm.odTPJZMI77N1XB80R6pr0Q"
-
+REFRESH_TOKEN = "eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.DoI6R0eRPicaJySArxDLXYWbpwk_YAuYo_YIBaAS950oojWCyCxHd3d2_KrHgBRDBq-bNOgQQBvJUCMsMcfnOdE2zf2WTa8gKPAC-6FHYE4Mnz2UaMdWYUWh_MvU55s2W5ldNS5tteETOEsGGjRrYyriLx3VVEvuJIO9LLROQK40CKLMJnBIniVyG7Y3Ni6-IEALnX91H6qK2INN6gRPEPL9JKi2GW6N9xrUdr1Kq6q57CuaPyC-i90ir2HBznWvxbHLgV8GVRO-ROaJu0oGaCc9zEhSwQIqbcvd2kz1MaxMBpbbqfuQP-PYVmiXVKHMBUmHdVig8XNF1Ae4pLpO4g.dWlYRU39CEjQksbk.1e9Zs8MWhM4cVxNYNj6VcB5Z-CWeZp4ns622qpUjwFConM95034m6YJzEda2qRyECHm4SwAgscbX-LWUs5a6z_NOrASqIZgWvBjIm_KGJtn7dAwCo1JXHxeITwJTtvabDTtFq1DsSKt6JaY7CzaXDkTJoI9XTUrttzdRMqbKi2_zmqpxfBn25WLqcTVm_AHi3DI6pcUJXeJbBuYe7-lF2Bgky5Qrm5l2S98DVePzu1UWRaooy5Zk_g3g_cJgZ6h3fG9DRMiE4H0Qt6A-kCz_Xyy3VjmV6NdVXsytrXg4BwsAsMp1Du2AZug6feSsbpyqfAmfTOMjpttNvqpjWp6lZsdq_6QEaQpTA0aPRxJb5CV4o2bd9HDUP77Knc1McZWQrLtnNK7zuuZMTmoxbVlPcG0kvvBogRQXuwVYy4LSL6ddDjvntOjQgnDBO-_Q2G4vr63XWQoRJPc7h9b6TCIUG-zbPN7uuOENxg2D2Cu1KPvwdfbqNlhMuReENk228br9HagaE3ujcG8oGLQyMLmJUtMSgaUGbRHqaDj21IxrxUcvt8-mgV0lbiSigCx6CMN0CBgCXrts3KC7VZ5mkGDuxKx5xitNoBov1aBmht9VSX9o8RjSj7zb0KJxUHWFphF9tAxr5_8m-vmXxH5rEKjEtuSOZD4VaQiJw__3tVV37qaLZO8uW2kHm9ezreSckCyXHGB49cdaEVWXC8XWmMu-NX_4mzTRYHfVe6wtwmWmQhZWN2yhA6gTm8NpH0Es4ARSrl6fHyGXUHBbUZ9DgD0l-86LOS5UUHAdf0oViGIEqPppWhp82r3YKD_6Zas1RL5AoZYSPpNmAqf9o_MRxsaPCaqzcy_DpLe3q1k4bfepsOQQwEt37h_lhdyyVA34xJtckkdFksnwUgW7dXgjuu-mLX17A7jpuTppSL_O_-ri-WL68EASQwbXjBfs0wwAsz5ME_114fxqsMaWzvWVwcCImeiQ3PPldwn0wIoug-neBC2Q7vY5f-oAiEOlkerNn3zG4An-E6-9dyBghTIpjo5PgCb6IStSliT7XaR-gPSNb4WpNzPeDCNdRaeqh2FQolKIfSaxpeOOSw34I4rqUeyCUPZ18YkXM-YXdyM0Q7VqX6lUPnXT3o32CMCp8hYS99oYfqLqz2bw9vBqVgGm1kZH7SPB-4uzQ_cuUgemZ85MglIh0qCNdnlbFS7iq8sxhZCMcWX3aGfF3J1Oku_6dl-BxWZ5YrA_O3H4TLrN3j-9bRpnRHNqidERDNjBC0IrmPSpWejGDrhIaLu3cNRThWn_fCN2TsqO52aKER7bGd9Bsumc4MrRUWAgxxrqP0E.jx5chvPTME7hKuAJINnMfg"
 repo_client = OmixAtlas(REFRESH_TOKEN)
 
-### Get All Omixatlases
+# Get All Omixatlases
 
-all_omixatlases = repo_client.get_all_omixatlas()
+# all_omixatlases = repo_client.get_all_omixatlas()
 
-with open(LOG_FILE, "a") as logfile:
-    logfile.write("\n\nAll Omixatlases")
-    logfile.write(dumps(all_omixatlases, indent=4))
+# with open(LOG_FILE, "a") as logfile:
+#     logfile.write("\n\nAll Omixatlases")
+#     logfile.write(dumps(all_omixatlases, indent=4))
 
-### Get Omixatlas by Name
+# Get Omixatlas by Name
 
-repo_name = "elucidata.test_omixwiki"
+repo_name = "elucidata.liveromix_atlas"
 
-repo_details = repo_client.omixatlas_summary(repo_name)
+# repo_details = repo_client.omixatlas_summary(repo_name)
 
-with open(LOG_FILE, "a") as logfile:
-    logfile.write("\n\n Omixatlas Details with Name\n")
-    logfile.write(dumps(repo_details, indent=4))
+# with open(LOG_FILE, "a") as logfile:
+#     logfile.write("\n\n Omixatlas Details with Name\n")
+#     logfile.write(dumps(repo_details, indent=4))
 
-### Get Omixatlas by ID
+# Get Omixatlas by ID
 
-repo_id = "1624966495340"
+repo_id = "1615965444377"
 
-repo_details = repo_client.omixatlas_summary(repo_name)
+# repo_details = repo_client.omixatlas_summary(repo_name)
 
-with open(LOG_FILE, "a") as logfile:
-    logfile.write("\n\n Omixatlas Details with ID\n")
-    logfile.write(dumps(repo_details, indent=4))
+# with open(LOG_FILE, "a") as logfile:
+#     logfile.write("\n\n Omixatlas Details with ID\n")
+#     logfile.write(dumps(repo_details, indent=4))
 
-### SQL Queries
+# SQL Queries
 
-query = "SELECT * FROM liver_atlas_files WHERE disease.keyword = 'Obesity'"
+query = "SELECT file_type FROM liveromix_atlas_files LIMIT 100,2000"  # datasets
 
+query = "SELECT * FROM liveromix_atlas_gct_metadata"  # samples
+
+query = "SELECT * FROM liveromix_atlas_gct_data"  # features
+
+query = "SELECT * FROM liveromix_atlas_h5ad_data"  # features single cell
+
+query = "SELECT * FROM liveromix_atlas_h5ad_metadata"  # features single cell
+
+query = "SELECT file_Type FROM liveromix_atlas_files"  # semantic analysis exception
+
+query = "SELECT file_type FROM liveromix_atas_files"  # index not found exception
+
+query = "SELECT file_type FROM liveromix_atlas_files LIMIT 20000"  # invalid limit
+
+# forbidden, only select allowed
+query = "DELETE file_type FROM liveromix_atlas_files"
+
+# query_response = repo_client.query_metadata(query)
+
+# print(query_response)
+
+# with open(LOG_FILE, "a") as logfile:
+#     logfile.write("\n\n Metadata Query\n")
+#     try:
+#         logfile.write(query_response.to_string())
+#     except AttributeError:
+#         logfilete(dumps(query_response, indent=4))
+
+
+# Download Data
+
+query = "SELECT dataset_id FROM liveromix_atlas_files"
 query_response = repo_client.query_metadata(query)
 
-with open(LOG_FILE, "a") as logfile:
-    logfile.write("\n\n Metadata Query\n")
-    logfile.write(query_response.to_string())
+print(query_response)
 
+# with open(LOG_FILE, "a") as logfile:
+#     logfile.write("\n\n Metadata Query\n")
+#     logfile.write(dumps(search_response, indent=4))
 
-### Download Data
+# repo_name = 'elucidata.liver_atlas'
 
-query = {
-    "query": {
-        "bool": {
-            "must": [
-                {"term": {"_index": "test_files"}},
-                {"term": {"kw_filetype.keyword": "png"}},
-            ]
-        }
-    }
-}
-
-search_response = repo_client.search_metadata(query)
-
-with open(LOG_FILE, "a") as logfile:
-    logfile.write("\n\n Metadata Query\n")
-    logfile.write(dumps(search_response, indent=4))
-
-data_id = search_response["hits"]["hits"][0]["_id"]
+data_id = query_response.iloc[0]
 
 response = repo_client.download_data(repo_name, data_id)
 signed_url = response.get("data")
