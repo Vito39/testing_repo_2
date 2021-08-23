dbb938fa70dce843c26f7894e55fb20673c5426b
gaisensei <88478345+gaisensei@users.noreply.github.com>
2021-08-23 16:08:24 +0530

    Update session.py

--- polly/session.py
+++ polly/session.py
@@ -1,11 +1,13 @@
 from requests import Session
+import pkg_resources
 
 class PollySession(Session):
     def __init__(self, REFRESH_TOKEN):
         Session.__init__(self)
+        version = pkg_resources.get_distribution('polly-python').version
 
         self.headers = {
             "Content-Type": "application/vnd.api+json",
             "Cookie" : f"refreshToken={REFRESH_TOKEN}",
-            "User-Agent" : "polly-python/0.0.5",
+            "User-Agent" : "polly-python/"+version,
         }
