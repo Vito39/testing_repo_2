3149659140ad110cafbe0de0d05ab21c21a54776
Vivek Mathpal <vivek.mathpal@elucidata.io>
2021-09-06 16:47:24 +0530

    resolving linting

--- polly/polly.py
+++ polly/polly.py
@@ -5,15 +5,16 @@ class UnauthorizedException(Exception):
     def __str__(self):
         return "Authorization failed as credentials not found. Please try again."
 
+
 class Polly:
     default_session = None
-    
+
     @classmethod
-    def auth(cls,token):
+    def auth(cls, token):
         cls.default_session = PollySession(token)
 
     @classmethod
-    def get_session(cls,token=None):
+    def get_session(cls, token=None):
         if not token:
             if not cls.default_session:
                 raise UnauthorizedException
