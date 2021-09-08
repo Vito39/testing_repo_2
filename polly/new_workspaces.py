caafb2786f47a1cf5eebc241f8cb515c9f104b7a
Vivek Mathpal <vivek.mathpal@elucidata.io>
2021-09-08 18:50:07 +0530

    Added functionalities for workspaces

---
+++ polly/new_workspaces.py
@@ -0,0 +1,26 @@
+from polly.session import PollySession
+from pprint import pprint
+from polly.errors import error_handler
+import json
+
+
+class Workspaces():
+    def __init__(self, refresh_token=None) -> None:
+        self.base_url = 'https://v2.api.polly.elucidata.io/workspaces'
+        self.session = PollySession(refresh_token)
+
+    def create_workspace(self,name: str,description: str):
+        url = self.base_url
+        payload = {"data":{"type":"workspaces","attributes":{"name":name,"description":description,"project_property":{"type":"workspaces","labels":""}}}}
+        response = self.session.post(url, data = json.dumps(payload))
+        error_handler(response)
+        attributes = response.json()['data']['attributes']
+        print(f'Workspace Created ! \n Workspace Name = {attributes["name"]} and Workspace ID = {attributes["id"]}')
+
+    def fetch_my_workspaces(self):
+        url = self.base_url
+        response = self.session.get(url)
+        error_handler(response)
+        for workspace in response.json()['data']:
+            details=workspace['attributes']
+            print(f'Workspace-Name = {details["name"]} and Workspace ID = {details["id"]}')
