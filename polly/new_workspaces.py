aea33b974944733adb839b40b2356fe1df820602
Vivek Mathpal <88478345+gaisensei@users.noreply.github.com>
2021-09-14 16:31:46 +0530

    Update new_workspaces.py

--- polly/new_workspaces.py
+++ polly/new_workspaces.py
@@ -1,11 +1,14 @@
 from polly.session import PollySession
 from polly.errors import error_handler
+from polly.constants import V2_API_ENDPOINT
+import logging
+import pandas as pd
 import json
 
 
 class Workspaces():
     def __init__(self, refresh_token=None) -> None:
-        self.base_url = 'https://v2.api.polly.elucidata.io/workspaces'
+        self.base_url = f'{V2_API_ENDPOINT}/workspaces'
         self.session = PollySession(refresh_token)
 
     def create_workspace(self, name: str, description=None):
@@ -18,14 +21,14 @@ class Workspaces():
         response = self.session.post(url, data=json.dumps(payload))
         error_handler(response)
         attributes = response.json()['data']['attributes']
-        print(f'Workspace Created ! \n Workspace Name = {attributes["name"]} and Workspace ID = {attributes["id"]}')
-        return True
+        logging.basicConfig(level=logging.INFO)
+        logging.info('Workspace Created !')
+        return attributes
 
     def fetch_my_workspaces(self):
         url = self.base_url
         response = self.session.get(url)
         error_handler(response)
-        for workspace in response.json()['data']:
-            details = workspace['attributes']
-            print(f'Workspace-Name = {details["name"]} and Workspace ID = {details["id"]}')
-        return True
+        pd.set_option("display.max_columns", 20)
+        dataframe = pd.DataFrame.from_dict(pd.json_normalize(response.json()['data']), orient='columns')
+        return dataframe
