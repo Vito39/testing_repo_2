d7e7703ccc69e96f3d28df25eaa49f9a6c28d31f
mayank_saurabh <mayank.saurabh@elucidata.io>
2022-03-09 11:11:39 +0530

    [WIP]:linting changes

--- polly/index_schema_level_conversion_const.py
+++ polly/index_schema_level_conversion_const.py
@@ -1,2 +1,6 @@
 # index and level name
-indexes_schema_level_map = {"files":"dataset", "gct_metadata": "sample", "h5ad_metadata": "sample"}
+indexes_schema_level_map = {
+    "files": "dataset",
+    "gct_metadata": "sample",
+    "h5ad_metadata": "sample",
+}
